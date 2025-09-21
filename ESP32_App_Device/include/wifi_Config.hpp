0ef25e55382f866eebc186e9bfe267f5153094f1
Javier de la Concepción Dorado <javi.conce.2000@gmail.com>
2025-09-21 18:53:28 +0200

    Implementation wifi mechanisims for client

---
+++ ESP32_App_Device/include/wifi_Config.hpp
@@ -0,0 +1,8 @@
+
+#include <WiFi.h>
+
+const std::string ssid = "Manager_app_V1"; // Replace with your network SSID
+const std::string password = "Manager_app_V1"; // Replace with your network password
+const int MAX_CONNECTION_ATTEMPTS = 10;
+
+int startWIFI_CONFIG();
--- ESP32_App_Device/src/main.cpp
+++ ESP32_App_Device/src/main.cpp
@@ -1,18 +1,16 @@
 #include <Arduino.h>
-
-// put function declarations here:
-int myFunction(int, int);
+#include "wifi_Config.hpp"
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(115200);
+  delay(1000);
+  int ret = startWIFI_CONFIG();
+  if (ret != 1) {
+    Serial.println("[MAIN][ERROR] Failed to connect to WiFi.");
+  } else {
+    Serial.println("[MAIN][INFO] WiFi connected successfully.");
+  }
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
 }
